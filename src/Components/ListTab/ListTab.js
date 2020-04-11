739a9d7b594eef15e21921e15acbd46c14c2ef31
Yggdrasil09 <karthik99th.tnk@gmail.com>
2020-04-11 18:54:32 +0530

    Added loading bar

--- src/Components/ListTab/ListTab.js
+++ src/Components/ListTab/ListTab.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import { Row, Col, Tabs, Form, Button, Select, Alert, Table, Modal, Input, message, Typography, Popconfirm } from 'antd';
+import { Row, Col, Tabs, Form, Button, Select, Alert, Table, Modal, Input, message, Typography, Popconfirm, Spin } from 'antd';
 import { DeleteOutlined } from '@ant-design/icons';
 
 import './ListTab.css';
@@ -41,13 +41,15 @@ class ListTab extends Component{
             quantity:0,
             selected_quantity:0,
             table:[],
-            jsonlist:[],
             visible:false,
             canteen_data:[],
             selected_canteen:"",
             selected_name:"",
             selected_feedback:"",
             itemId:"",
+            submit_visible:false,
+            submit_order_details:[],
+            loading:false,
         };
         this.columns = [
             {
@@ -72,7 +74,7 @@ class ListTab extends Component{
                 key: 'totalprice',
             },
             {
-                title: 'operation',
+                title: 'Delete',
                 dataIndex: 'operation',
                 render: (text, record) =>
                   this.state.table.length >= 1 ? (
@@ -109,14 +111,15 @@ class ListTab extends Component{
             let data = {
                 Name:this.state.selected_name,
                 Zone:this.state.selected_canteen,
-                Order:this.state.jsonlist,
+                Order:this.state.table,
                 Feedback:this.state.selected_feedback,
             }
             console.log(data);
             this.setState({
                 visible: false,
+                loading:true,
             },()=>{
-                fetch("http://localhost:5000/submit-order",{
+                fetch("http://groceryappv1-env.eba-fbstmdmg.us-east-2.elasticbeanstalk.com/submit-order",{
                     method:"POST",
                     body: JSON.stringify(data),
                     headers: {
@@ -126,7 +129,15 @@ class ListTab extends Component{
                     credentials: "same-origin"
                 })
                 .then(res=>{
-                    window.location.reload();
+                    return res.json();
+                })
+                .then(data=>{
+                    console.log(data);
+                    this.setState({
+                        loading:false,
+                        submit_order_details:data,
+                        submit_visible:true,
+                    })
                 })
                 .catch(err=>{
                     console.log(err);
@@ -173,21 +184,15 @@ class ListTab extends Component{
         this.setState({
             kart:this.state.kart.concat([values])
         },()=>{
-            console.log(this.state.kart)
             this.setState({
                 table:this.state.table.concat([{
-                    key:this.state.table.length+1,
+                    key:this.state.kart.length+1,
                     item:this.state.item,
                     quantity:this.state.selected_quantity,
                     price:this.state.price,
                     totalprice:(this.state.price*this.state.selected_quantity).toFixed(2),
-                }]),
-                jsonlist:this.state.jsonlist.concat([{
-                    item:this.state.item,
-                    quantity:this.state.quantity,
-                    price:this.state.price,
                     code:this.state.itemId,
-                }])
+                }]),
             })
         })
     };
@@ -230,8 +235,16 @@ class ListTab extends Component{
         });
     };
 
+    handleSubmit = e => {
+        this.setState({
+            submit_visible:false,
+        },()=>{
+            window.location.reload();
+        })
+    }
+
     componentWillMount(){
-        fetch("http://localhost:5000/test",{
+        fetch("http://groceryappv1-env.eba-fbstmdmg.us-east-2.elasticbeanstalk.com/test",{
             method:"GET"
         })
         .then(res=>{
@@ -246,7 +259,7 @@ class ListTab extends Component{
         .catch(err=>{
             console.log(err);
         })
-        fetch("http://localhost:5000/trial",{
+        fetch("http://groceryappv1-env.eba-fbstmdmg.us-east-2.elasticbeanstalk.com/trial",{
             method:"GET"
         })
         .then(res=>{
@@ -267,92 +280,108 @@ class ListTab extends Component{
         const { mode } = this.state.mode;
         return(
             <div>
-                <Row>
-                    <Col xs={1} sm={2} md={1} lg={1} xl={1}/>
-                    <Col xs={22} sm={20} md={22} lg={22} xl={22}>
-                        <Title level={2}>NSC Canteen Online Delivery Service</Title>
-                        <Title level={3}>Contact: +91-8082992508</Title>
-                    </Col>
-                    <Col xs={1} sm={2} md={1} lg={1} xl={1}/>
-                </Row>
-                <Row >
-                    <Col xs={2} sm={2} md={2} lg={2} xl={1}/>
-                    <Col xs={20} sm={20} md={20} lg={20} xl={22}>
-                        <Tabs defaultActiveKey="1" tabPosition={mode} onChange={this.onTabChange} size='large'>
-                            {Object.keys(this.state.data).map(i => (
-                                <TabPane tab={`${i}`} key={i}>
-                                    <Form {...layout} ref={this.formRef} name="control-ref" onFinish={this.onFinish}>
-                                        <Form.Item name="Item" label="Select from list and enter the quantity" rules={[{required: true,},]}>
-                                            <Select placeholder="Select a item" onChange={this.onItemChange} allowClear>
-                                                {this.state.data[i].map(j => (
-                                                    <Option value={j["Item Name"]} key={j["Item Name"]}>{j["Item Name"]}</Option>
-                                                ))}
-                                            </Select>
-                                        </Form.Item>
-                                        <Alert className="alert-mesg" message={"Price per unit : "+this.state.price+"/-"} type="info" />
-                                        <Form.Item name="Quantity" label="Select the quantity" rules={[{required: true,},]}>
-                                            <Select  placeholder="Select the quantity of item" onChange={this.onAmountChange} allowClear>
-                                                {Object.keys(this.state.quantity).map(j => (
-                                                    <Option value={this.state.quantity[j]} key={this.state.quantity[j]}>{this.state.quantity[j]}</Option>
-                                                ))}
-                                            </Select>
-                                        </Form.Item>
-                                        <Form.Item {...tailLayout}>
-                                            <Button type="primary" htmlType="submit" >
-                                                Submit
-                                            </Button>
-                                            <Button className="checkout_but" htmlType="button" onClick={this.showModal}>
-                                                Check Out
-                                            </Button>
-                                            <Modal title="Confirm your Details to place Order" visible={this.state.visible} onOk={this.handleOk} onCancel={this.handleCancel}>
-                                                <Form layout={"vertical"} ref={this.formRef} name="control-ref" >
-                                                    <Form.Item  {...null} name="Canteen" label="Please select your desired drop point and time" rules={[{required: true,},]}>
-                                                        <Select placeholder="Select a time" onChange={this.onCanteenChange} allowClear>
-                                                            {Object.keys(this.state.canteen_data).map(i => (
-                                                                Object.keys(this.state.canteen_data[i]).map(j => (
-                                                                    <Option value={i+"!"+this.state.canteen_data[i][j]} key={i+this.state.canteen_data[i][j]}>{i+" : "+this.state.canteen_data[i][j]}</Option>
-                                                                ))
-                                                            ))}
-                                                        </Select>
-                                                    </Form.Item>
-                                                    <Form.Item name="name"  label="Please enter your name" rules={[{required: true,},]}>
-                                                        <Input onChange={this.onNameChange} placeholder="Enter your name"/>
-                                                    </Form.Item>
-                                                    <Form.Item name="feedback"  label="Please enter your feedback">
-                                                        <TextArea onChange={this.onFeedbackChange} placeholder="Enter your feedback" rows={4} />
-                                                    </Form.Item>
-                                                </Form>
-                                            </Modal>
-                                        </Form.Item>
-                                    </Form>
-                                </TabPane>
-                            ))}
-                        </Tabs>
-                    </Col>
-                    <Col xs={2} sm={2} md={2} lg={2} xl={1}/>
-                </Row>
-                <Row style={{ marginTop:"20px"}}>
-                    <Col xs={2} sm={2} md={4} lg={4} xl={4}/>
-                    <Col xs={20} sm={20} md={16} lg={16} xl={16}>
-                        <Table columns={this.columns} dataSource={this.state.table} pagination={{ pageSize: 4 }}/>
-                    </Col>
-                    <Col xs={2} sm={2} md={4} lg={4} xl={4}/>
-                </Row>
-                <Row>
-                    <Col xs={2} sm={2} md={2} lg={2} xl={2}/>
-                    <Col xs={20} sm={20} md={20} lg={20} xl={20}>
-                        <Paragraph>
-                            NSC is commencing home area delivery of CSD items in a few officers accommodation where there is no other CSD counter nearby. The item list has been kept to essentials. The same will be revised regularly as per availability of stock and suggestions from dependents. Feel free to call the officer incharge at +91-8082992508
-                        </Paragraph>
-                        <Paragraph>
-                            The orders placed will be delivered the next day as per times displayed. 3 ₹ will be charged over and above the billing amount for packing material. Please give the exact amount to the delivery boy. This will avoid crowding and avoidable delays in the process.
-                        </Paragraph>
-                        <Paragraph>
-                            Stay In : Stay Safe !!!
-                        </Paragraph>
-                    </Col>
-                    <Col xs={2} sm={2} md={2} lg={2} xl={2}/>
-                </Row>
+                <Spin size="large" spinning={this.state.loading}>
+                    <Row>
+                        <Col xs={1} sm={2} md={1} lg={1} xl={1}/>
+                        <Col xs={22} sm={20} md={22} lg={22} xl={22}>
+                            <Title level={2}>NSC Canteen Online Delivery Service</Title>
+                            <Title level={3}>Contact: +91-8082992508</Title>
+                        </Col>
+                        <Col xs={1} sm={2} md={1} lg={1} xl={1}/>
+                    </Row>
+                    <Row >
+                        <Col xs={2} sm={2} md={2} lg={2} xl={1}/>
+                        <Col xs={20} sm={20} md={20} lg={20} xl={22}>
+                            <Tabs defaultActiveKey="1" tabPosition={mode} onChange={this.onTabChange} size='large'>
+                                {Object.keys(this.state.data).map(i => (
+                                    <TabPane tab={`${i}`} key={i}>
+                                        <Form {...layout} ref={this.formRef} name="control-ref" onFinish={this.onFinish}>
+                                            <Form.Item name="Item" label="Select from list and enter the quantity" rules={[{required: true,},]}>
+                                                <Select placeholder="Select a item" onChange={this.onItemChange} allowClear>
+                                                    {this.state.data[i].map(j => (
+                                                        <Option value={j["Item Name"]} key={j["Item Name"]}>{j["Item Name"]}</Option>
+                                                    ))}
+                                                </Select>
+                                            </Form.Item>
+                                            <Alert className="alert-mesg" message={"Price per unit : "+this.state.price+"/-"} type="info" />
+                                            <Form.Item name="Quantity" label="Select the quantity" rules={[{required: true,},]}>
+                                                <Select  placeholder="Select the quantity of item" onChange={this.onAmountChange} allowClear>
+                                                    {Object.keys(this.state.quantity).map(j => (
+                                                        <Option value={this.state.quantity[j]} key={this.state.quantity[j]}>{this.state.quantity[j]}</Option>
+                                                    ))}
+                                                </Select>
+                                            </Form.Item>
+                                            <Form.Item {...tailLayout}>
+                                                <Button type="primary" htmlType="submit" >
+                                                    Submit
+                                                </Button>
+                                                <Button className="checkout_but" htmlType="button" onClick={this.showModal}>
+                                                    Check Out
+                                                </Button>
+                                                <Modal title="Confirm your Details to place Order" visible={this.state.visible} onOk={this.handleOk} onCancel={this.handleCancel}>
+                                                    <Form layout={"vertical"} ref={this.formRef} name="control-ref" >
+                                                        <Form.Item  {...null} name="Canteen" label="Please select your desired drop point and time" rules={[{required: true,},]}>
+                                                            <Select placeholder="Select a time" onChange={this.onCanteenChange} allowClear>
+                                                                {Object.keys(this.state.canteen_data).map(i => (
+                                                                    Object.keys(this.state.canteen_data[i]).map(j => (
+                                                                        <Option value={i+"!"+this.state.canteen_data[i][j]} key={i+this.state.canteen_data[i][j]}>{i+" : "+this.state.canteen_data[i][j]}</Option>
+                                                                    ))
+                                                                ))}
+                                                            </Select>
+                                                        </Form.Item>
+                                                        <Form.Item name="name"  label="Please enter your name" rules={[{required: true,},]}>
+                                                            <Input onChange={this.onNameChange} placeholder="Enter your name"/>
+                                                        </Form.Item>
+                                                        <Form.Item name="feedback"  label="Please enter your feedback">
+                                                            <TextArea onChange={this.onFeedbackChange} placeholder="Enter your feedback" rows={4} />
+                                                        </Form.Item>
+                                                    </Form>
+                                                </Modal>
+                                                <Modal title="Your Order Details" visible={this.state.submit_visible} onOk={this.handleSubmit} onCancel={this.handleSubmit} footer={[]}>
+                                                    <p>
+                                                        Your order has been placed. Your order id is {this.state.submit_order_details[0]}. 
+                                                    </p>
+                                                    <p>
+                                                        Kindly meet our representative at {this.state.submit_order_details[2]} during {this.state.submit_order_details[3]}. 
+                                                    </p>
+                                                    <p>
+                                                        Total price : {this.state.submit_order_details[1]}
+                                                    </p>
+                                                    <p>
+                                                        * Rs.3 packaging charges has been added.
+                                                    </p>
+                                                </Modal>
+                                            </Form.Item>
+                                        </Form>
+                                    </TabPane>
+                                ))}
+                            </Tabs>
+                        </Col>
+                        <Col xs={2} sm={2} md={2} lg={2} xl={1}/>
+                    </Row>
+                    <Row style={{ marginTop:"20px"}}>
+                        <Col xs={2} sm={2} md={4} lg={4} xl={4}/>
+                        <Col xs={20} sm={20} md={16} lg={16} xl={16}>
+                            <Table columns={this.columns} dataSource={this.state.table} pagination={{ pageSize: 4 }}/>
+                        </Col>
+                        <Col xs={2} sm={2} md={4} lg={4} xl={4}/>
+                    </Row>
+                    <Row>
+                        <Col xs={2} sm={2} md={2} lg={2} xl={2}/>
+                        <Col xs={20} sm={20} md={20} lg={20} xl={20}>
+                            <Paragraph>
+                                NSC is commencing home area delivery of CSD items in a few officers accommodation where there is no other CSD counter nearby. The item list has been kept to essentials. The same will be revised regularly as per availability of stock and suggestions from dependents. Feel free to call the officer incharge at +91-8082992508
+                            </Paragraph>
+                            <Paragraph>
+                                The orders placed will be delivered the next day as per times displayed. 3 ₹ will be charged over and above the billing amount for packing material. Please give the exact amount to the delivery boy. This will avoid crowding and avoidable delays in the process.
+                            </Paragraph>
+                            <Paragraph>
+                                Stay In : Stay Safe !!!
+                            </Paragraph>
+                        </Col>
+                        <Col xs={2} sm={2} md={2} lg={2} xl={2}/>
+                    </Row>
+                </Spin>
             </div>
         );
     }
